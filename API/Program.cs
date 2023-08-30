427f09ab437e3654444700ba0e2f992766f45451
TianshuXin <1090062661l@gmail.com>
2023-08-29 22:05:25 -0500

    Add Initial setup

---
+++ API/Program.cs
@@ -0,0 +1,20 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<StoreContext>(opt =>{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+var app = builder.Build();
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
